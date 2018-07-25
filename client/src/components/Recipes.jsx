a19431de01245756843ab495bcbe1223366b9b3f
Alex <ajhollid@gmail.com>
2018-07-24 17:20:37 -0700

    Add fetching for recipes to main reicpe page

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { List } from 'semantic-ui-react';
+import axios from 'axios';
 
 class Recipes extends Component {
-  render() {
-    return (
-      <p>{this.props.rootPath}</p>
-    );
+  constructor( props ) {
+    super( props );
+    this.state = {
+      recipes: [],
+    };
+  }
+  componentDidMount() {
+    const pageMap = {
+      recipes: () => '/api/recipes',
+      myrecipes: () => '/api/1/recipes/',
+      random: () => '/api/recipes/random',
+    };
+
+    const fn = pageMap[this.props.rootPath];
+    if ( fn ) {
+      axios.get( fn() )
+        .then( ( baseRecipeResponse ) => {
+          console.log( baseRecipeResponse );
+        } );
+    }
   }
 }
 
