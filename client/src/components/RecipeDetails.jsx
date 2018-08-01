e3751119d08a5616dfb3d6d943372a4f8cc837a1
Alex <ajhollid@gmail.com>
2018-07-31 19:56:46 -0700

    Update /api/recipe/:recipe_id to return entire recipe including ingredients and directions

--- client/src/components/RecipeDetails.jsx
+++ client/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 import axios from 'axios';
 
 
@@ -24,24 +24,26 @@ class RecipeDetails extends Component {
   }
   render() {
     return (
-      <Grid stackable columns="equal">
-        <Grid.Row>
-          <Grid.Column >
-            <Image size="medium" centered src={this.state.recipe.image_url} />
-          </Grid.Column>
-          <Grid.Column className="green-column">
-             2
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column className="green-column">
+      <div>
+        <Grid stackable columns="equal">
+          <Grid.Row>
+            <Grid.Column >
+              <Image size="medium" centered src={this.state.recipe.image_url} />
+            </Grid.Column>
+            <Grid.Column className="green-column">
+              <Header textAlign="center">{this.state.recipe.name}</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column className="green-column">
             3
-          </Grid.Column>
-          <Grid.Column>
+            </Grid.Column>
+            <Grid.Column>
              4
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
