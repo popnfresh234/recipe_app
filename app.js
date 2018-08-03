1da0c07f11aff06127d54e728a69f60b116d02bf
Alex <ajhollid@gmail.com>
2018-08-03 11:40:59 -0700

    Add AWS to server

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require( 'path' );
 const cookieParser = require( 'cookie-parser' );
 const cookieSession = require( 'cookie-session' );
 const logger = require( 'morgan' );
-
+require( 'dotenv' ).config();
 const indexRouter = require( './routes/index' );
 const recipesRouter = require( './routes/recipes' );
+const aws = require( 'aws-sdk' );
 
 
 const app = express();
@@ -34,6 +35,12 @@ app.use( '/api/recipes', recipesRouter );
 // app.use( ( req, res, next ) => {
 //   next( createError( 404 ) );
 // } );
+
+const s3 = new aws.S3( { accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET, region: 'us-west-2' } );
+const params = { Bucket: 'big-cooking-recipe-images', Key: 'images/myimage.jpg', ContentType: 'image/jpeg' };
+s3.getSignedUrl( 'putObject', params, ( err, url ) => {
+  console.log( 'Your generated pre-signed URL is', url );
+} );
 
 
 module.exports = app;
