5e7938d94cffd45260c0ee8388c670551ccc41d7
Alex <ajhollid@gmail.com>
2018-08-22 12:00:37 -0700

    Increase char limit of direction

--- db/migrations/20180719180550_create-user-table.js
+++ db/migrations/20180719180550_create-user-table.js
@@ -30,7 +30,7 @@ exports.up = function ( knex, Promise ) {
 
     knex.schema.createTable( 'directions', ( table ) => {
       table.increments( 'id' ).primary();
-      table.string( 'description' ).notNull();
+      table.string( 'description', 10000 ).notNull();
       table.integer( 'recipe_id' ).references( 'id' ).inTable( 'recipes' ).onDelete( 'CASCADE' );
     } ),
 
